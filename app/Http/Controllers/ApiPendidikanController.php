6f381b234811f9b80a459e326803a1b6bde893a8
ainsley215 <ainsley02121@gmail.com>
2025-03-21 08:32:36 +0700

    acara 22

--- app/Http/Controllers/ApiPendidikanController.php
+++ app/Http/Controllers/ApiPendidikanController.php
@@ -15,4 +15,39 @@ class ApiPendidikanController extends Controller
         $pendidikan = Pendidikan::all();
         return Response::json($pendidikan);
     }
+
+    public function getPen($id) {
+        $pendidikan = Pendidikan::find($id);
+        if (!$pendidikan) {
+            return Response::json([
+                'status' => 'error',
+                'message' => 'Data not found'
+            ], 404);
+        }
+        return Response::json($pendidikan);
+    }
+
+    public function createPen(Request $request) {
+        $pendidikan = Pendidikan::create($request->all());
+        return Response::json([
+            'status' => 'ok',
+            'message' => 'Pendidikan created'
+        ], 201);
+    }
+
+    public function updatePen($id, Request $request) {
+        Pendidikan::find($id)->update($request->all());
+        return Response::json([
+            'status' => 'ok',
+            'message' => 'Pendidikan updated'
+        ], 201);
+    }
+
+    public function deletePen($id) {
+        Pendidikan::destroy($id);
+        return Response::json([
+            'status' => 'ok',
+            'message' => 'Pendidikan deleted'
+        ], 201);
+    }
 }
